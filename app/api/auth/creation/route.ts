dc4d46e8a9a87ea48dafd4e9559c281b75483ef4
NeoMH91 <neomenghwee1991@hotmail.com>
2024-08-16 11:11:15 +0800

    added new vercel redirect link

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -47,5 +47,9 @@ export async function GET() {
       },
     });
   }
-  return NextResponse.redirect("http://localhost:3000");
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://digital-marketplace-chi.vercel.app"
+  );
 }
